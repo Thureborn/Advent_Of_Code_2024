6fac1d010593a1970138e9e14b86b153f6a7306e
Oscar Thureborn <oscar.thureborn@quokka.com>
2024-12-13 14:19:10 +0100

    solution day 7 part 2

--- src/day07/solutionDay07.ts
+++ src/day07/solutionDay07.ts
@@ -2,23 +2,23 @@ import * as fs from 'fs'
 
 const filePath = 'input_files/inputDay07.txt'
 
-function solution1(data: string[][]): number {
+function solution1(equations: string[][]): number {
 
   let sumOfSolvableEquations = 0
-  for (let i = 0; i < data.length; i++) {
-    const numberOfOperations = data[i].length - 2
+  for (let i = 0; i < equations.length; i++) {
+    const numberOfOperations = equations[i].length - 2
     for (let j = 0; j < 2 ** numberOfOperations; j++) {
       const operations = j.toString(2).padStart(numberOfOperations, '0')
-      let sum = Number(data[i][1].valueOf())
+      let sum = Number(equations[i][1].valueOf())
       for (let k = 0; k < operations.length; k++) {
         if (operations[k] == '0') {
-          sum += Number(data[i][k + 2])
+          sum += Number(equations[i][k + 2])
         }
         else {
-          sum *= Number(data[i][k + 2])
+          sum *= Number(equations[i][k + 2])
         }
       }
-      if (Number(data[i][0]) == sum) {
+      if (Number(equations[i][0]) == sum) {
         sumOfSolvableEquations += sum
         break;
       }
@@ -27,9 +27,32 @@ function solution1(data: string[][]): number {
   return sumOfSolvableEquations
 }
 
-function solution2(): number {
-  let numberOfSafeLevels = 0
-  return numberOfSafeLevels
+function solution2(equations: string[][]): number {
+
+  let sumOfSolvableEquations = 0
+  for (let i = 0; i < equations.length; i++) {
+    const numberOfOperations = equations[i].length - 2
+    for (let j = 0; j < 3 ** numberOfOperations; j++) {
+      const operations = j.toString(3).padStart(numberOfOperations, '0')
+      let sum = Number(equations[i][1].valueOf())
+      for (let k = 0; k < operations.length; k++) {
+        if (operations[k] == '0') {
+          sum += Number(equations[i][k + 2])
+        }
+        else if (operations[k] == '1') {
+          sum *= Number(equations[i][k + 2])
+        }
+        else {
+          sum = Number(sum.toString() + equations[i][k + 2])
+        }
+      }
+      if (Number(equations[i][0]) == sum) {
+        sumOfSolvableEquations += sum
+        break;
+      }
+    }
+  }
+  return sumOfSolvableEquations
 }
 
 export async function solveDay7() {
@@ -42,10 +65,9 @@ export async function solveDay7() {
 
     const lines = data.trim().split('\n')
 
-    const input = lines.map(line => (line.split(': ')
+    const equations = lines.map(line => (line.split(': ')
       .flatMap(part => part.split(' '))))
 
-    // const input = [['20', '4', '4', '4', '4', '4']]
-    console.log('Answer to question #1: ' + solution1(input) + ' Answer to question #2: ' + solution2())
+    console.log('Answer to question #1: ' + solution1(equations) + ' Answer to question #2: ' + solution2(equations))
   });
 }
