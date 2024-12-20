976f98738eaf7e91f50b694b22ba9a3a83fb3846
Oscar Thureborn <oscar.thureborn@quokka.com>
2024-12-20 12:20:53 +0100

    solution for day 12 part 1

--- src/day12/solutionDay12.ts
+++ src/day12/solutionDay12.ts
@@ -2,8 +2,71 @@ import * as fs from 'fs'
 
 const filePath = 'input_files/inputDay12.txt'
 
-function solution1(): number {
-  return 0
+interface Field {
+  type: string
+  area: Set<string>;
+  perimeter: number
+}
+
+function replaceChar(string: string, char: string, index: number) {
+  return string.substring(0, index) + char + string.substring(1 + index)
+}
+
+function floodSearch(map: string[], field: Field, x: number, y: number): Field {
+  if (field.area.has(JSON.stringify([x, y]))) {
+    return field
+  }
+  if (y < 0 || y >= map.length) {
+    field.perimeter += 1
+    return field
+  }
+  if (x < 0 || x >= map[y].length) {
+    field.perimeter += 1
+    return field
+  }
+  if (map[y][x] != field.type) {
+    field.perimeter += 1
+    return field
+  }
+
+  field.area.add(JSON.stringify([x, y]))
+  map[y] = replaceChar(map[y], '.', x)
+
+  floodSearch(map, field, x + 1, y)
+  floodSearch(map, field, x - 1, y)
+  floodSearch(map, field, x, y + 1)
+  floodSearch(map, field, x, y - 1)
+
+  return field
+}
+
+function solution1(map: string[]): number {
+
+  const fields = new Map<number, Field>()
+  let fieldId = 0
+  for (let y = 0; y < map.length; y++) {
+    for (let x = 0; x < map[y].length; x++) {
+      if (map[y][x] != '.') {
+        const field: Field = {
+          type: map[y][x],
+          area: new Set<string>(),
+          perimeter: 0
+
+        }
+        floodSearch(map, field, x, y)
+        fields.set(fieldId, { ...field })
+        fieldId++
+      }
+    }
+  }
+
+
+  let sum = 0
+  fields.forEach((field, _) => {
+    sum += [...field.area].length * field.perimeter
+  })
+
+  return sum
 }
 
 function solution2(): number {
@@ -20,6 +83,6 @@ export async function solveDay12() {
 
     const map = data.trim().split('\r\n')
 
-    console.log('Answer to question #1: ' + solution1() + ' Answer to question #2: ' + solution2())
+    console.log('Answer to question #1: ' + solution1(map) + ' Answer to question #2: ' + solution2())
   });
 }
